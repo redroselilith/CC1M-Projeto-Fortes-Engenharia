3d8cc7215399f23189f85334c533b4f9d89a7a22
Luiz Filipe Coutinho Borges <31438808+Mallahowl@users.noreply.github.com>
2023-11-30 12:42:37 -0300

    script for dynamic font size in the title

---
+++ adjustFontSize.js
@@ -0,0 +1,27 @@
+function adjustFontSize() {
+    const container = document.getElementById('title');
+    const resizeText = container.querySelector('h1');
+    const containerWidth = container.offsetWidth;
+    const containerHeight = container.offsetHeight;
+
+    const maxWidth = containerWidth * 0.9; 
+    const maxHeight = containerHeight * 0.9;
+
+    // Tamanho inicial da fonte. Nao usar número quebrado.
+    let fontSize = 48;
+    fontSize = parseInt(fontSize);
+    let textFits = false;
+
+    while (!textFits) {
+      resizeText.style.fontSize = fontSize + 'px';
+      if (resizeText.scrollWidth <= maxWidth && resizeText.scrollHeight <= maxHeight) {
+        textFits = true;
+      } else {
+        fontSize--;
+      }
+    }
+  }
+
+  // Call the function initially and on window resize
+  adjustFontSize();
+  window.addEventListener('resize', adjustFontSize);
